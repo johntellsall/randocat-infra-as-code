0f07ec940017b1b85ba9ca7de812302d34a40f97
John Mitchell <john@johntellsall.com>
2019-03-16 11:51:56 -0700

    wip: adding simple database

---
+++ terraform/database.tf
@@ -0,0 +1,89 @@
+# database.tf
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+# data "aws_subnet_ids" "all" {
+#   vpc_id = "${data.aws_vpc.default.id}"
+# }
+
+# data "aws_security_group" "default" {
+#   vpc_id = "${data.aws_vpc.default.id}"
+#   name   = "default"
+# }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+#   storage_type         = "gp2"
+  engine               = "postgresql"
+#   engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+#   parameter_group_name = "default.mysql5.7"
+}
+
+# resource "aws_rds_cluster" "postgresql" {
+#   cluster_identifier      = "mar16-aurora-cluster"
+#   engine                  = "aurora-postgresql"
+#   availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+#   database_name           = "mydb"
+#   master_username         = "foo"
+#   master_password         = "bar"
+#   backup_retention_period = 5
+#   preferred_backup_window = "07:00-09:00"
+# }
+
+
+# module "db" {
+#   source = "../../"
+
+#   identifier = "demodb"
+
+#   engine            = "postgres"
+#   engine_version    = "9.6.3"
+#   instance_class    = "db.t2.large"
+#   allocated_storage = 5
+#   storage_encrypted = false
+
+#   # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
+#   name = "demodb"
+
+#   # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+#   # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+#   # user cannot be used as it is a reserved word used by the engine"
+#   username = "demouser"
+
+#   password = "YourPwdShouldBeLongAndSecure!"
+#   port     = "5432"
+
+#   vpc_security_group_ids = ["${data.aws_security_group.default.id}"]
+
+#   maintenance_window = "Mon:00:00-Mon:03:00"
+#   backup_window      = "03:00-06:00"
+
+#   # disable backups to create DB faster
+#   backup_retention_period = 0
+
+#   tags = {
+#     Owner       = "user"
+#     Environment = "dev"
+#   }
+
+#   # DB subnet group
+#   subnet_ids = ["${data.aws_subnet_ids.all.ids}"]
+
+#   # DB parameter group
+#   family = "postgres9.6"
+
+#   # DB option group
+#   major_engine_version = "9.6"
+
+#   # Snapshot name upon DB deletion
+#   final_snapshot_identifier = "demodb"
+
+#   # Database Deletion Protection
+#   deletion_protection = true
+# }
--- terraform/network.tf
+++ terraform/network.tf
@@ -4,15 +4,15 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   version = "1.59.0"
 
-  name = "jm-mar16-vpc"
+  name = "mar16-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
-#   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+  # for secure Lambdas (no internet routing)
+  # - intra_subnets = ["10.0.1.0/24"]
+  database_subnets= ["10.0.20.0/24", "10.0.21.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
